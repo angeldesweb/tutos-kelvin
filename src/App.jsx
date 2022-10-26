5e5a43e57499ae83d5fc149c65c8a8c21f0f18dd
angeldesweb <angeldesweb@gmail.com>
2022-10-25 21:09:13 -0400

    Clase 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Header } from './lib/home/components/Header';
 import './App.css';
 import { ListBox } from './lib/home/components/ListBox';
-// Componentes presentacionales , Componentes lógicos
 
 export const App = () => {
     return (
